d338135dadd046663af6f7ac0fbf5fc8a1cddb07
Kira65218 <Kirambrunner@gmail.com>
2026-07-27 16:16:47 -0700

    Changes from July 27th

---
+++ Index.js
@@ -0,0 +1,9 @@
+var background = document.getElementById("core");
+
+function activate() {
+    background.style.backgroundColor = "Black";
+}
+
+background.onclick = function change() {
+    background.style.backgroundColor = "Lightcoral"
+}
